786c631cff73fcd09e34cfc78d378f647a57e6fb
alciesielka <alexandraciesielka@gmail.com>
2024-03-03 06:16:15 -0500

    FOR MILDNESS

--- AppFolder/app.py
+++ AppFolder/app.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file, Response, Blueprint, redirect, url_for
+import subprocess
+from bicep_curls import infer
 
 app = Flask(__name__)
+
+about_blueprint = Blueprint('about', __name__)
+resources_blueprint = Blueprint('resources', __name__)
+menst_blueprint = Blueprint('menst', __name__)
+
 
 @app.route('/')
 def index():
@@ -14,5 +21,18 @@ def about():
 @app.route('/resources')
 def resources():
     return render_template('resources.html')
+
+@app.route('/menst')
+def menst():
+    return render_template('resources2.html')
+
+@app.route('/bicep_curls')
+def video_feed():
+    infer()
+    # Redirect back to the home page after the function completes
+    return redirect(url_for('index'))
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
